fb4fe2e58777c2b012bd37471c9fee5f677e577c
Koboti Mogale <coffart@gmail.com>
2020-02-26 19:43:27 +0200

    Added main class and test class, .gitigore and pom file.

---
+++ src/main/java/za/co/koboti/mssqlexample/MssqlExampleApplication.java
@@ -0,0 +1,13 @@
+package za.co.koboti.mssqlexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MssqlExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MssqlExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/koboti/mssqlexample/MssqlExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.koboti.mssqlexample;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MssqlExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
